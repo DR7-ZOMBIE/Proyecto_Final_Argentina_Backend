fa5fd578bebf166def91c690aac0c3615885c1c4
agustin1796 <hernan1796@gmail.com>
2024-03-02 21:58:18 -0300

    agrego funcionalidad basica de spring security para usuarios y admin

---
+++ src/main/java/com/Alquiler/Alquiler_Vehiculo/security/AppConfig.java
@@ -0,0 +1,46 @@
+package com.Alquiler.Alquiler_Vehiculo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class AppConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN") // Requiere el rol "ADMIN" para acceder a las URLs que comienzan con "/admin/"
+                .antMatchers("/user/**").hasRole("USER") // Requiere el rol "USER" para acceder a las URLs que comienzan con "/user/"
+                .anyRequest().authenticated() // Requiere autenticación para cualquier otra URL
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
